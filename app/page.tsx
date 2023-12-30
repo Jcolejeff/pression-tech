9cd7153f3d6cb1dfdaa45b0b7d1cd216cd78bf04
ikwuh jeffery enokela <ikwuhjefferye@gmail.com>
2023-12-30 10:04:49 +0100

    initial project set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,37 @@
-import { allPosts } from "@/.contentlayer/generated"
-import Link from "next/link"
+import Link from "next/link";
+
+const allPosts = [
+	{
+		_id: 1,
+		slug: "posts/post-1",
+		title: "Post 1",
+		description: "This is the first post",
+	},
+	{
+		_id: 2,
+		slug: "posts/post-2",
+		title: "Post 2",
+		description: "This is the second post",
+	},
+	{
+		_id: 3,
+		slug: "posts/post-3",
+		title: "Post 3",
+		description: "This is the third post",
+	},
+];
 
 export default function Home() {
-  return (
-    <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
-    </div>
-  )
+	return (
+		<div className="prose dark:prose-invert">
+			{allPosts.map((post) => (
+				<article key={post._id}>
+					<Link href={post.slug}>
+						<h2>{post.title}</h2>
+					</Link>
+					{post.description && <p>{post.description}</p>}
+				</article>
+			))}
+		</div>
+	);
 }
