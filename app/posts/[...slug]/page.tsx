138fea3dbe8c93961c759fdadc3006f5d79dc64b
ikwuh jeffery enokela <ikwuhjefferye@gmail.com>
2023-12-30 12:00:19 +0100

    feat : added prettier

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -3,94 +3,96 @@ import { notFound } from "next/navigation";
 import { Metadata } from "next";
 
 const allPosts = [
-	{
-		slugAsParams: "post-1",
-		title: "Post 1",
-		description: "This is the first post",
-		body: {
-			code: "console.log('Hello, World!');",
-		},
-	},
-	{
-		slugAsParams: "post-2",
-		title: "Post 2",
-		description: "This is the second post",
-		body: {
-			code: "console.log('Welcome to the blog!');",
-		},
-	},
-	{
-		slugAsParams: "post-3",
-		title: "Post 3",
-		description: "This is the third post",
-		body: {
-			code: "console.log('Welcome to the blog!');",
-		},
-	},
-	{
-		slugAsParams: "post-4",
-		title: "Post 4",
-		description: "This is the fourth post",
-		body: {
-			code: "console.log('Welcome to the blog!');",
-		},
-	},
+   {
+      slugAsParams: "post-1",
+      title: "Post 1",
+      description: "This is the first post",
+      body: {
+         code: "console.log('Hello, World!');",
+      },
+   },
+   {
+      slugAsParams: "post-2",
+      title: "Post 2",
+      description: "This is the second post",
+      body: {
+         code: "console.log('Welcome to the blog!');",
+      },
+   },
+   {
+      slugAsParams: "post-3",
+      title: "Post 3",
+      description: "This is the third post",
+      body: {
+         code: "console.log('Welcome to the blog!');",
+      },
+   },
+   {
+      slugAsParams: "post-4",
+      title: "Post 4",
+      description: "This is the fourth post",
+      body: {
+         code: "console.log('Welcome to the blog!');",
+      },
+   },
 ];
 
 interface PostProps {
-	params: {
-		slug: string[];
-	};
+   params: {
+      slug: string[];
+   };
 }
 
 async function getPostFromParams(params: any) {
-	const slug = params?.slug?.join("/");
-	const post = allPosts.find((post) => post.slugAsParams === slug);
+   const slug = params?.slug?.join("/");
+   const post = allPosts.find((post) => post.slugAsParams === slug);
 
-	if (!post) {
-		null;
-	}
+   if (!post) {
+      null;
+   }
 
-	return post;
+   return post;
 }
 
 export async function generateMetadata({ params }: any): Promise<any> {
-	const post = await getPostFromParams(params);
+   const post = await getPostFromParams(params);
 
-	if (!post) {
-		return {};
-	}
+   if (!post) {
+      return {};
+   }
 
-	return {
-		title: post.title,
-		description: post.description,
-	};
+   return {
+      title: post.title,
+      description: post.description,
+   };
 }
 
 export async function generateStaticParams(): Promise<any> {
-	return allPosts.map((post) => ({
-		slug: post.slugAsParams.split("/"),
-	}));
+   return allPosts.map((post) => ({
+      slug: post.slugAsParams.split("/"),
+   }));
 }
 
 export default async function PostPage({ params }: any) {
-	const post = await getPostFromParams(params);
+   const post = await getPostFromParams(params);
 
-	if (!post) {
-		notFound();
-	}
+   if (!post) {
+      notFound();
+   }
 
-	return (
-		<article className="py-6 prose dark:prose-invert">
-			<h1 className="mb-2">{post.title}</h1>
-			{post.description && (
-				<p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
-					{post.description}
-				</p>
-			)}
-			<hr className="my-4" />
-			<p>{post.body.code}</p>
-			{/* <Mdx code={post.body.code} /> */}
-		</article>
-	);
+   return (
+      <article className="prose py-6 dark:prose-invert">
+         <h1 className="mb-2">{post.title}</h1>
+         {post.description && (
+            <p className="mt-0 text-xl text-slate-700 dark:text-slate-200 md:px-2 xl:pb-6 2xl:p-6 ">
+               {post.description}
+            </p>
+         )}
+         <hr className="my-4" />
+
+         <p>{post.body.code}</p>
+
+         {/* <Mdx code={post.body.code} /> */}
+      </article>
+   );
 }
