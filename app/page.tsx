42e8f40179eecd980cfed4d17a8a1f35a23fc95c
kela <ikwuhjefferye@gmail.com>
2024-02-28 00:30:56 +0100

    fix : fixed cache error

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ import { Input } from "@/components/ui/input";
 import { wordPressInstance, baseUrl } from "@/lib/http";
 
 // export const dynamic = "force-dynamic";
-// export const fetchCache = "force-no-store";
+export const fetchCache = "force-no-store";
 // export const revalidate = 0;
 export default async function Home() {
    const heroData = await wordPressInstance.posts().perPage(7).page(1).embed();
